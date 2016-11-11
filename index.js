71dd64ddb7389c8985bcd62c2cb6c42932f09909
Smulligan85 <sean.mulligan85@gmail.com>
2016-11-10 19:04:19 -0800

    Add map functions

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+const sortedRobots = robots.map(function (robot) {
+  const decepticonTrue = knownDecepticons.includes(robot.name);
+  return Object.assign({}, robot, {
+    alliance: decepticonTrue ? 'decepticon' : 'autobot'
+  });
+});
+
+const coloredZebraStripes = zebraStripes.map(function (zebra, index) {
+  const isEvenStripe = index % 2 === 0;
+  return Object.assign({}, zebra, {
+    color: isEvenStripe ? 'black' : 'white'
+  });
+});
